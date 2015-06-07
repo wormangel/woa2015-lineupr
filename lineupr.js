ef40366ae1fbf5937af0c888b817e97e2e3af9bb
wormangel <wormangel@gmail.com>
2015-06-07 19:02:19 -0300

    tab navigation, styling changes

--- lineupr.js
+++ lineupr.js
@@ -1,7 +1,25 @@
-
-
 $(document).ready(function(){
     $('.running-order').addClass('container');
+
+    var day_selector = $('<ul></ul>').addClass('nav nav-tabs').attr('role', 'tablist');
+    $.each(schedule.program, function(i, val){
+        var tab_date = $('<li></li>').attr('role', 'presentation');
+        if (i == 0) {
+            tab_date.addClass('active');
+        }
+        var tab_link = $('<a></a>').attr('href', '#ro' + val.event_date).attr('role', 'tab').attr('data-toggle', 'tab').attr('aria-controls', 'ro' + val.event_date);
+        tab_link.text(new Date(val.event_date).toDateString());
+        
+        tab_date.append(tab_link);
+        day_selector.append(tab_date);
+    });
+
+    $('.running-order').append(day_selector);
+
+  
+    // Create the content container
+    var container = $('<div class="tab-content"></div>');
+    $('.running-order').append(container);
 
     // Create the Hours div
   
@@ -15,48 +33,51 @@ $(document).ready(function(){
         
         div_hours.append(div_hour);
     }
-    $('.running-order').append(div_hours);
+    container.append(div_hours);
   
-    // Create the Stages div
-    var div_stages = $('<div></div>').addClass('stages');
-    $.each(schedule.program[0].running_order, function(i, val) {
-        var div_stage = $('<div></div>').addClass('stage').attr('data-name', val.stage_name);
+    // For each day of festival
+    $.each(schedule.program, function(i, event_day) {
+        // Create the container for the day
+        var div_event_day = $('<div></div>').attr('role', 'tabpanel').addClass('tab-pane').attr('id', 'ro' + event_day.event_date);
+        if (i == 0) {
+            div_event_day.addClass('active');
+        }
         
-        var h5_title = $('<h5></h5>').addClass('stage-title').text(val.stage_name);
-        div_stage.append(h5_title);
+        // Create the Stages div
+        var div_stages = $('<div></div>').addClass('stages');
+        $.each(event_day.running_order, function(i, val) {
+            var div_stage = $('<div></div>').addClass('stage').attr('data-name', val.stage_name);
+        
+            var h5_title = $('<h5></h5>').addClass('stage-title').text(val.stage_name.toUpperCase());
+            div_stage.append(h5_title);
       
-        div_stages.append(div_stage);
-    });
-  
-    $('.running-order').append(div_stages);
-  
-    // Create the div_artists container
-    $('.stage').each(function(){
-        var div_artists = $('<div></div>').addClass('artists').appendTo($(this));
-    });
-  
-    // Now for the hard work!
-    $.each(schedule.program[0].running_order, function(i,stage){
-        $.each(stage.acts, function(i, act) {
-            var div_act = $('<div></div>').addClass('artist container text-center btn');
-            var p_act = $('<p></p>');
-            var span_act = $('<span></span>');
-            var strong_act = $('<strong></strong>').text(act.act).append('<br>');
-            var small_time = $('<small></small>').append(hour_friendly(act.start_time)).append(' - ').append(hour_friendly(act.end_time));
-            strong_act.append(small_time);
-            span_act.append(strong_act);
-            p_act.append(span_act);
-            div_act.append(p_act);
-            div_act.css('top', $('.hour[data-time="' + hour_round(act.start_time) + '"]').offset().top + minutes_offset(act.start_time));
-            div_act.css('height', act_duration(act.start_time, act.end_time) * 2);
-            div_act.css('width', $('.stage[data-name="'+ stage.stage_name +'"] .artists').width());
-            
-            $('.stage[data-name="'+ stage.stage_name +'"] .artists').append(div_act);
-            
+            div_stages.append(div_stage);
         });
-    });
   
-    
+        div_event_day.append(div_stages);
+  
+        // Now for the hard work!
+        $.each(event_day.running_order, function(i,stage){
+            $.each(stage.acts, function(i, act) {
+                var div_act = $('<div></div>').addClass('artist container text-center');
+                var p_act = $('<p></p>');
+                var span_act = $('<span></span>');
+                var strong_act = $('<strong></strong>').text(act.act).append('<br>');
+                var small_time = $('<small></small>').append(hour_friendly(act.start_time)).append(' - ').append(hour_friendly(act.end_time));
+                strong_act.append(small_time);
+                span_act.append(strong_act);
+                p_act.append(span_act);
+                div_act.append(p_act);
+                div_act.css('top', $('.hour[data-time="' + hour_round(act.start_time) + '"]').offset().top + minutes_offset(act.start_time));
+                div_act.css('height', act_duration(act.start_time, act.end_time) * 2);
+                div_act.css('width', $('.stage[data-name="'+ stage.stage_name +'"] .artists').width());
+            
+                div_event_day.find('.stage[data-name="'+ stage.stage_name +'"]').append(div_act);
+            });
+        });
+        
+        container.append(div_event_day);
+    });
 });
 
 // Helpers
